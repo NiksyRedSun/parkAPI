dcf7be0dab0f4d4f635a4e69e4c032fcf9442863
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2024-06-22 16:52:16 +0300

    Half BD
    Похоже на то, что половина работы с БД сделана, далее будут ситуативные изменения

--- models.py
+++ models.py
@@ -1,34 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.orm import Mapped
+from typing import List
 
 
 db = SQLAlchemy()
 
 
 class Resident(db.Model):
+
     __tablename__ = 'residents'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    second_name = db.Column(db.String(50))
-    surname = db.Column(db.String(50))
-    pas_series = db.Column(db.Integer)
-    pas_number = db.Column(db.Integer)
+
+    first_name = db.Column(db.String(50), nullable=False)
+    second_name = db.Column(db.String(50), nullable=False)
+    surname = db.Column(db.String(50), nullable=False)
+    pas_series = db.Column(db.Integer, nullable=False)
+    pas_number = db.Column(db.Integer, nullable=False)
+
+    cars: Mapped[List["Car"]] = db.relationship(back_populates="resident")
+    parking_slots: Mapped[List["ParkingSlot"]] = db.relationship(back_populates="resident")
+    apartments: Mapped[List["Apartment"]] = db.relationship(back_populates="resident")
 
     def __repr__(self):
         return f"resident_id: {self.id}, resident_name: {self.name}"
 
 
 class Apartment(db.Model):
+
     __tablename__ = 'apartments'
 
     id = db.Column(db.Integer, primary_key=True)
-    resident_id = db.Column(db.Integer)
-    num = db.Column(db.Integer)
+    resident_id = db.Column(db.ForeignKey('residents.id', ondelete="CASCADE"), nullable=False)
+
+    num = db.Column(db.Integer, nullable=False)
+
+    resident: Mapped["Resident"] = db.relationship(back_populates="apartments")
 
 
     def __repr__(self):
         return f"apartment_id: {self.id}"
+
+
+class Car(db.Model):
+
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    resident_id = db.Column(db.ForeignKey('residents.id', ondelete="CASCADE"), nullable=False)
+
+    brand = db.Column(db.String(50), nullable=False)
+    model = db.Column(db.String(50), nullable=False)
+    plate = db.Column(db.String(50), nullable=False)
+
+    resident: Mapped["Resident"] = db.relationship(back_populates="cars")
+
+    def __repr__(self):
+        return f"car_id: {self.id}"
+
+
+class ParkingSlot(db.Model):
+
+    __tablename__ = 'parking_slots'
+
+    id = db.Column(db.Integer, primary_key=True)
+    resident_id = db.Column(db.ForeignKey('residents.id', ondelete="CASCADE"), nullable=False)
+
+    num = db.Column(db.Integer, nullable=False)
+    letter = db.Column(db.String(1), nullable=False)
+
+    resident: Mapped["Resident"] = db.relationship(back_populates="parking_slots")
+
+    def __repr__(self):
+        return f"parking_slot: {self.id}"
 
 
 

a325b0e890ccb842c8ab65b279185f3e1db64c4f
NiksyRedSun <112153281+NiksyRedSun@users.noreply.github.com>
2024-06-23 20:45:58 +0300

    Validation
    Валидация всех данных сделана с помощью регулярных выражений и чатаДЖПТ

--- forms.py
+++ forms.py
@@ -1,24 +1,40 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField, TextAreaField, IntegerField, SelectMultipleField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange
+from wtforms.validators import DataRequired, Email, Length, EqualTo, NumberRange, Regexp
 
 
 # в конце нужно будет добавить валидаторы:
 
+
 class NewResidentForm(FlaskForm):
-    last_name = StringField("Фамилия:", validators=[Length(min=4, max=100, message="Должен быть от 4 до 100 символов")])
-    first_name = StringField("Имя:")
-    patronymic = StringField("Отчество:")
-    pas_series = StringField("Серия паспорта:")
-    pas_number = StringField("Номер паспорта:")
+    last_name = StringField("Фамилия:", validators=[Length(min=2, max=30, message="Фамилия должна быть от 2 до 50 символов"),
+                                                    Regexp("^[А-Яа-яЁё]+([-'][А-Яа-яЁё]+)*$",
+                                                           message="Фамилия может состоять только из кирилических "
+                                                                   "символов, включать в себя дефис и апострофы")])
+
+    first_name = StringField("Имя:", validators=[Length(min=2, max=30, message="Имя должно быть от 2 до 50 символов"),
+                                                 Regexp("^[А-Яа-яЁё]+([-'][А-Яа-яЁё]+)*$",
+                                                        message="Имя может состоять только из кирилических "
+                                                                "символов, включать в себя дефис и апострофы")])
+
+    patronymic = StringField("Отчество:", validators=[Length(min=2, max=30, message=" Отчество должно быть от 2 до 50 символов"),
+                                                      Regexp("^[А-Яа-яЁё]+([-'][А-Яа-яЁё]+)*$",
+                                                             message="Отчество может состоять только из кирилических "
+                                                                     "символов, включать в себя дефис и апострофы")
+                                                      ])
+
+    pas_series = StringField("Серия паспорта:", validators=[Regexp("^\d{2} \d{2}$",
+                                                                   message="Серия паспорта указывается в формате 00 00")])
+
+    pas_number = StringField("Номер паспорта:", validators=[Regexp("^\d{6}$",
+                                                                   message="Номер паспорта указывается в формате 000000")])
     submit = SubmitField("Добавить")
 
 
 class NewCarForm(FlaskForm):
-    model = StringField("Модель:", validators=[Length(min=4, max=100, message="Должен быть от 4 до 100 символов")])
-    plate = StringField("Номер:")
+    model = StringField("Модель:", validators=[Length(min=4, max=100, message="Модель быть от 4 до 100 символов")])
+    plate = StringField("Номер:", validators=[Regexp("^[А-Я]\d{3}[А-Я]{2}$", message="Номер автомобиля указывается в формате А555АА")])
     submit = SubmitField("Добавить")
-
 
 
 def TakeParkingSlotForm(free_slots):
@@ -48,74 +64,3 @@ def TakeApartmentForm(apartments):
     form = TakeApartmentForm()
     return form
 
-
-class RegisterForm(FlaskForm):
-    name = StringField("Логин: ", validators=[Length(min=4, max=100, message="Должен быть от 4 до 100 символов")])
-    # email = StringField("Email: ", validators=[Email("Некорректный email")])
-    psw = PasswordField("Пароль: ", validators=[DataRequired(), Length(min=4, max=100, message="Пароль должен быть от 4 до 100 символов")])
-    psw2 = PasswordField("Пароль повторно: ", validators=[DataRequired(), EqualTo("psw", message="Пароли не совпадают")])
-    submit = SubmitField("Регистрация")
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField("Поле", validators=[Length(min=30, max=300, message="Должен быть от 30 до 300 символов")])
-    submit = SubmitField("Загрузить")
-
-
-class GetCharForm(FlaskForm):
-    id = IntegerField("ID персонажа", validators=[DataRequired()])
-    submit = SubmitField("Подключить персонажа")
-
-
-def DeletePostsForm(posts):
-    class DeletePostForm(FlaskForm):
-        pass
-
-    DeletePostForm.submit = SubmitField("Удалить посты")
-    ids = {}
-    for post in posts:
-        ids["id" + str(post.id)] = post.id
-        postname = f"ID поста: {post.id} Автор: {post.usr}\n Текст: {post.text}"
-        setattr(DeletePostForm, "id" + str(post.id), BooleanField(postname, default=False))
-
-    form = DeletePostForm()
-    return form, ids
-
-
-
-def ItemsPostForm():
-
-    class ItemsPostForm(FlaskForm):
-        pass
-    setattr(ItemsPostForm, f"itemName", StringField(f"Название вещи", validators=[Length(min=4, max=100, message="Должно быть от 4 до 100 символов")]))
-    setattr(ItemsPostForm, f"itemMaxHp", IntegerField(f"Подъем hp", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]))
-    setattr(ItemsPostForm, f"itemAttack", IntegerField(f"Подъем атаки", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]))
-    setattr(ItemsPostForm, f"itemDefense", IntegerField(f"Подъем защиты", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]))
-    setattr(ItemsPostForm, f"itemInitiative", IntegerField(f"Подъем ловкости", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]))
-    setattr(ItemsPostForm, f"forAttack", BooleanField(f"Используется для атаки", default=False))
-    setattr(ItemsPostForm, f"submit", SubmitField(f"Обновить вещь"))
-    setattr(ItemsPostForm, f"itemId", None)
-
-    form = ItemsPostForm()
-    return form
-
-
-# def ItemsPostForm(num):
-#
-#
-#     form = type(f"ItemsPostForm{num}", (FlaskForm, ), {
-#
-#         # data members
-#         f"itemName": StringField(f"Название вещи", validators=[Length(min=4, max=100, message="Должно быть от 4 до 100 символов")]),
-#         f"itemMaxHp": IntegerField(f"Подъем hp", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]),
-#         f"itemAttack": IntegerField(f"Подъем атаки", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]),
-#         f"itemDefense": IntegerField(f"Подъем защиты", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]),
-#         f"itemInitiative": IntegerField(f"Подъем ловкости", validators=[NumberRange(min=0, message="Не должно быть меньше 0")]),
-#         f"forAttack": BooleanField(f"Используется для атаки", default=False),
-#         f"submit{num}": SubmitField(f"Обновить вещь {num + 1}"),
-#         f"formNum": num,
-#         f"itemId": None,
-#     })
-#
-#
-#     return form
